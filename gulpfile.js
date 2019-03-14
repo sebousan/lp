9487e3f2f97a2d968079702bfc09d8df1dcd078f
Sebastien Moulene <sebousan@gmail.com>
2019-03-14 17:37:57 +0100

    Add image compression and css/js uglify

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,15 @@ var config = require('./config.json');
 var bs = require('browser-sync').create();
 
 // assets
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer'); // autoprefixer for css
 var browserify = require('gulp-browserify');
+var cleanCSS = require('gulp-clean-css'); // css uglify
+var image = require('gulp-image'); // image compression
 var plumber = require('gulp-plumber');
+var rename = require('gulp-rename'); // rename file for .min
 var sourcemaps = require('gulp-sourcemaps');
-var stylus = require('gulp-stylus');
+var stylus = require('gulp-stylus'); // stylus
+var uglify = require('gulp-uglify'); // js uglify
 
 // for include some partials
 var fileinclude = require('gulp-file-include');
@@ -25,6 +29,8 @@ var fileinclude = require('gulp-file-include');
 
 // BROWSER SYNC
 gulp.task("browser-sync", function () {
+    'use strict';
+
     bs.init({
         server: config.dist
     });
@@ -32,23 +38,26 @@ gulp.task("browser-sync", function () {
 
 // CSS
 gulp.task("styles", function () {
+    'use strict';
+
     return gulp
-        .src([config.style_folder + '/' + config.style])
+        .src(config.style_folder + '/' + config.style)
         .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(
-            stylus({
-                compress: true
-            })
-        )
+        .pipe(stylus())
         .pipe(autoprefixer(config.autoprefixer_options))
         .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.dist_css))
+        .pipe(rename(config.style_min))
+        .pipe(cleanCSS())
         .pipe(gulp.dest(config.dist_css))
         .pipe(bs.reload({stream: true})); // reload browser
 });
 
 // JS
 gulp.task('scripts', function () {
+    'use strict';
+
     return gulp
         .src([config.js_folder + '/' + config.js])
         .pipe(sourcemaps.init())
@@ -57,11 +66,17 @@ gulp.task('scripts', function () {
         }))
         .pipe(plumber())
         .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.dist_js))
+        .pipe(rename(config.js_min))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.dist_js))
         .pipe(bs.reload({stream: true})); // reload browser
 });
 
 // HTML
 gulp.task('html', function () {
+    'use strict';
+
     return gulp
         .src(config.html)
         .pipe(fileinclude({
@@ -74,18 +89,45 @@ gulp.task('html', function () {
 
 // FONTS
 gulp.task('fonts', function () {
+    'use strict';
+
     return gulp
         .src(config.font_folder + '/**/*')
         .pipe(gulp.dest(config.dist_font))
         .pipe(bs.reload({stream: true})); // reload browser
 });
 
+// SVG
+gulp.task('svg', function () {
+    'use strict';
+
+    return gulp
+        .src(config.svg_folder + '/**/*')
+        .pipe(image())
+        .pipe(gulp.dest(config.dist_svg))
+        .pipe(bs.reload({stream: true})); // reload browser
+});
+
+// IMAGES
+gulp.task('images', function () {
+    'use strict';
+
+    return gulp
+        .src(config.img_folder + '/**/*')
+        .pipe(image(config.image_options))
+        .pipe(gulp.dest(config.dist_img))
+        .pipe(bs.reload({stream: true})); // reload browser
+});
+
+
 // WATCH
 gulp.task("watch", ["browser-sync"], function () {
+    'use strict';
+
     gulp.watch([config.partial_folder + '/**/*.' + config.partial_ext], ['html']);
     gulp.watch([config.style_folder + '/**/*.' + config.style_ext], ['styles']);
     gulp.watch([config.js_folder + '/**/*.' + config.js_ext], ['scripts']);
 });
 
 // INIT
-gulp.task("default", ['styles', 'scripts', 'html', 'fonts']);
+gulp.task("default", ['styles', 'scripts', 'html', 'fonts', 'images']);
